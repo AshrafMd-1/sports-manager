e7667c2737a10f5049265491c60c7fb274d6e777
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-05-02 00:26:14 +0530

    add two personas clearance and their own set of authorizations

--- app.js
+++ app.js
@@ -26,10 +26,7 @@ app.use(cookieParser("cookie-parser-secret"));
 app.use(csurf("bxMNjNqSnWZvWE8f6oxTBykN71PoXmHz"));
 
 app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: "keyboard cat",
-    cookie: {
+    saveUninitialized: true, resave: true, secret: "keyboard cat", cookie: {
         maxAge: 3600000
     }
 }));
@@ -37,39 +34,33 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "email",
-            passwordField: "password",
+passport.use(new LocalStrategy({
+    usernameField: "email", passwordField: "password",
+}, (email, password, done) => {
+    User.findOne({
+        where: {
+            email,
         },
-        (email, password, done) => {
-            User.findOne({
-                where: {
-                    email,
-                },
-            })
-                .then(async (user) => {
-                    if (!user) {
-                        console.log("No User")
-                        return done(null, false);
-                    }
-                    const result = await bcrypt.compare(password, user.password);
-                    if (result) {
-                        console.log("Matched")
-                        return done(null, user);
-                    } else {
-                        console.log("Not Matched")
-                        return done(null, false);
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                    return done(err);
-                });
-        }
-    )
-);
+    })
+        .then(async (user) => {
+            if (!user) {
+                console.log("No User")
+                return done(null, false);
+            }
+            const result = await bcrypt.compare(password, user.password);
+            if (result) {
+                console.log("Matched")
+                return done(null, user);
+            } else {
+                console.log("Not Matched")
+                return done(null, false);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            return done(err);
+        });
+}));
 
 
 passport.serializeUser((user, done) => {
@@ -104,25 +95,25 @@ const isAdmin = (req, res, next) => {
     if (req.isAuthenticated()) {
         if (req.user.admin) {
             return next();
+        } else {
+            res.redirect('/dashboard')
         }
-        res.redirect('/logout')
+    } else {
+        res.redirect('/login')
     }
-    res.redirect('/login')
 }
 
 app.get('/', alreadyLoggedIn, async (req, res) => {
     console.log("Get Homepage")
     res.render('homepage', {
-        csrfToken: req.csrfToken(),
-        title: 'Homepage'
+        csrfToken: req.csrfToken(), title: 'Homepage'
     });
 });
 
 app.get('/signup', alreadyLoggedIn, (req, res) => {
     console.log("Get Signup")
     res.render('signup', {
-        csrfToken: req.csrfToken(),
-        title: 'Signup'
+        csrfToken: req.csrfToken(), title: 'Signup'
     });
 
 });
@@ -142,64 +133,56 @@ app.post('/signup', async (req, res) => {
 app.get('/login', alreadyLoggedIn, (req, res) => {
     console.log("Get Login")
     res.render('login', {
-        csrfToken: req.csrfToken(),
-        title: 'Login'
+        csrfToken: req.csrfToken(), title: 'Login'
     });
 });
 
 app.post('/login', passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
+    successRedirect: '/dashboard', failureRedirect: '/login',
 }));
 
 app.get('/dashboard', isLoggedIn, async (req, res) => {
     console.log("Get Dashboard")
-    if (req.user.admin) {
-        const createdSessions = await Session.getCreatedSessions(req.user.id)
-        const createdSports=[]
-        for (let i = 0; i < createdSessions.length; i++) {
-            const sport= await Sport.getSport(createdSessions[i].sportId)
-            createdSports.push(sport.dataValues.sport)
-        }
-        const joinedSessions = await Session.joinedSessions(req.user.id)
-        const joinedSports=[]
-        for (let i = 0; i < joinedSessions.length; i++) {
-            const sport= await Sport.getSport(joinedSessions[i].sportId)
-            joinedSports.push(sport.dataValues.sport)
-        }
-        res.render('dashboard', {
-            csrfToken: req.csrfToken(),
-            title: 'Dashboard',
-            createdSessions: createdSessions.map(session => session.dataValues),
-            createdSports: createdSports,
-            joinedSessions: joinedSessions.map(session => session.dataValues),
-            joinedSports: joinedSports,
-            dashboard: true
-
-        })
-    } else {
-        res.render('dashboard', {
-            csrfToken: req.csrfToken(),
-            title: 'Dashboard'
-        });
+    const createdSessions = await Session.getCreatedSessions(req.user.id)
+    const createdSports = []
+    for (let i = 0; i < createdSessions.length; i++) {
+        const sport = await Sport.getSport(createdSessions[i].sportId)
+        createdSports.push(sport.dataValues.sport)
     }
+    const joinedSessions = await Session.joinedSessions(req.user.id)
+    const joinedSports = []
+    for (let i = 0; i < joinedSessions.length; i++) {
+        const sport = await Sport.getSport(joinedSessions[i].sportId)
+        joinedSports.push(sport.dataValues.sport)
+    }
+    let admin = (req.user.admin)
+    let dashboard = true
+    res.render('dashboard', {
+        csrfToken: req.csrfToken(),
+        title: 'Dashboard',
+        createdSessions: createdSessions.map(session => session.dataValues),
+        createdSports: createdSports,
+        joinedSessions: joinedSessions.map(session => session.dataValues),
+        joinedSports: joinedSports,
+        dashboard: dashboard,
+        admin: admin
+    });
 });
 
 app.get('/sports', isLoggedIn, async (req, res) => {
     console.log("Get Sports")
     const sports = await Sport.findAll();
+    let admin = (req.user.admin)
     res.render('sports', {
-        csrfToken: req.csrfToken(),
-        title: 'Sports',
-        sports: sports.map(sport => sport.dataValues.sport)
+        csrfToken: req.csrfToken(), title: 'Sports', sports: sports.map(sport => sport.dataValues.sport), admin: admin
     });
 });
 
 app.get('/sports/new-sport', isAdmin, (req, res) => {
     console.log("Get New Sport")
+    let admin = (req.user.admin)
     res.render('new-sport', {
-        csrfToken: req.csrfToken(),
-        title: 'New Sport'
+        csrfToken: req.csrfToken(), title: 'New Sport', admin: admin
     });
 });
 
@@ -217,22 +200,24 @@ app.get('/sports/:sport', isLoggedIn, async (req, res) => {
     const sportId = await Sport.getSportId(req.params.sport)
     const oldSessions = await Session.getOlderSessions(sportId.dataValues.id)
     const newSessions = await Session.getNewerSessions(sportId.dataValues.id)
+    let admin = (req.user.admin)
+    let dashboard = false
     res.render('session', {
         csrfToken: req.csrfToken(),
-        title: `${req.params.sport} Sessions`,
+        title: `${req.params.sport} Sport`,
         sport: req.params.sport,
         oldSessions: oldSessions.map(session => session.dataValues),
         newSessions: newSessions.map(session => session.dataValues),
-        dashboard: false
-    })
+        admin: admin,
+        dashboard: dashboard
+    });
 });
 
 app.get('/sports/:sport/new-session', isLoggedIn, async (req, res) => {
     console.log(`Get New ${req.params.sport} Session`)
+    let admin = (req.user.admin)
     res.render('new-session', {
-        csrfToken: req.csrfToken(),
-        title: `New ${req.params.sport} Session`,
-        sport: req.params.sport
+        csrfToken: req.csrfToken(), title: `New ${req.params.sport} Session`, sport: req.params.sport, admin: admin
     });
 });
 
@@ -257,6 +242,7 @@ app.get('/sports/:sport/:id', isLoggedIn, async (req, res) => {
         const member = await User.getUserDetailsById(membersId[i])
         members.push(member.dataValues)
     }
+    let admin = (req.user.admin)
     res.render('session-info', {
         csrfToken: req.csrfToken(),
         title: `${req.params.sport} #${req.params.id} Session`,
@@ -265,7 +251,8 @@ app.get('/sports/:sport/:id', isLoggedIn, async (req, res) => {
         session: info.dataValues,
         members: members,
         userId: req.user.id,
-        old: (info.dataValues.date < new Date())
+        old: (info.dataValues.date < new Date()),
+        admin: admin
     })
 });
 
@@ -277,7 +264,7 @@ app.get('/sports/:sport/:id/:job', isLoggedIn, async (req, res) => {
     let membersId = await Session.getAllMembersId(req.params.id)
     let required = await Session.getRequired(req.params.id)
     if (req.params.job === "join-session") {
-        if (membersId.includes(req.user.id)) {
+        if (membersId.includes(req.user.id) || required <= 0) {
             res.redirect('/sports/' + req.params.sport + '/' + req.params.id)
             return
         }
@@ -302,12 +289,6 @@ app.get('/sports/:sport/:id/:job', isLoggedIn, async (req, res) => {
     res.redirect('/sports/' + req.params.sport + '/' + req.params.id)
 });
 
-
-app.get('/admin', isAdmin, (req, res) => {
-    res.send("Admin")
-
-});
-
 app.get("/logout", (req, res, next) => {
     req.logout((error) => {
         if (error) {
@@ -317,6 +298,5 @@ app.get("/logout", (req, res, next) => {
         res.redirect("/");
     });
 });
-
 
 module.exports = app
